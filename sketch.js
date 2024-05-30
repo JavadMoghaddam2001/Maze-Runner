659c00cecb65a722dc0f4639eb441ecd15ad2783
unknown <javadmoghaddam3511@gmail.com>
2024-05-30 11:01:57 +0330

    First commit

---
+++ sketch.js
@@ -0,0 +1,70 @@
+let cols, rows;
+let w = 40;
+let grid = [];
+let current;
+let stack = [];
+
+function setup() {
+  createCanvas(480, 480);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < cols; i++) {
+      grid.push(new Cell(i, j));
+    }
+  }
+
+  frameRate(5);
+  current = grid[0];
+}
+
+function draw() {
+  background(60);
+
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+
+  current.visited = true;
+  current.highlight();
+
+  let next = current.check();
+
+  if (next) {
+    next.visited = true;
+
+    stack.push(current);
+    removeWalls(current, next);
+
+    current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
+  }
+}
+
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+  return i + j * cols;
+}
+
+function removeWalls(a, b) {
+  let x = a.i - b.i;
+
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  let y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
